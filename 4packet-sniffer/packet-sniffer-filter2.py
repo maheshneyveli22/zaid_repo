60e86cc12c1e6395c1e2d6a1280b8195bd71181b
gs1-maheswarane <maheswaran.elumalai@expeditors.com>
2025-03-14 08:41:22 +0530

    packet sniffer

---
+++ 4packet-sniffer/packet-sniffer-filter2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import scapy.all as scapy
+
+
+def sniff(interface):#
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="https")
+
+#if we want to filter different things, we can give : "udp" or "port 21" for ftp, "arp", "tcp" , "port 80" for webservers 
+# now we need new module scapy-http to filter 
+
+
+def process_sniffed_packet(packet):
+    print(packet)
+
+# find interfaces in windows using power shell; by using command - netsh interface show interface
+#
+#
+# PS C:\Users\gs1-maheswarane> netsh interface show interface
+#
+# Admin State    State          Type             Interface Name
+# -------------------------------------------------------------------------
+# Enabled        Disconnected   Dedicated        Ethernet
+# Enabled        Connected      Dedicated        Ethernet 4
+# Enabled        Disconnected   Dedicated        Wi-Fi
+
+
+#sniff("Ethernet 4")
+sniff("Wi-Fi")
--- 4packet-sniffer/scapyreadme.txt
+++ 4packet-sniffer/scapyreadme.txt
@@ -1,1 +1,6 @@
 https://github.com/secdev/scapy/blob/master/README.md
+
+https://github.com/invernizzi/scapy-http
+
+Berkeley packet filter
+https://biot.com/capstats/bpf.html
