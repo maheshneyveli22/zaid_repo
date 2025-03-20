5fc527166003638255964e1ae8a588a1f0e113f3
gs1-maheswarane <maheswaran.elumalai@expeditors.com>
2025-03-20 08:44:53 +0530

    march20

---
+++ 5dns-spoofer/1netcut_anyclient.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import netfilterqueue
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.
+
+
+# This program can be used to cut internet connection for any client in the network
